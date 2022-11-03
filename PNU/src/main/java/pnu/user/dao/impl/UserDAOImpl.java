6cbef4a68ace449e424324530ca8ef93aeb38b29
tachna <86517819+tachna@users.noreply.github.com>
2022-11-03 23:21:08 +0900

    go2

--- PNU/src/main/java/pnu/user/dao/impl/UserDAOImpl.java
+++ PNU/src/main/java/pnu/user/dao/impl/UserDAOImpl.java
@@ -1,5 +1,12 @@
 package pnu.user.dao.impl;
 
-public class UserDAOImpl {
+import egovegramework.rte.psl.dataaccess.
+import pnu.user.dao.UserDAO;
 
+@Repository
+public class UserDAOImpl EgovAbstracMapper implements UserDAO{
+		@Override
+		public String selectPwd(String userId) {
+			return selectOne("User.selectPwd", userId)
+		}
 }
